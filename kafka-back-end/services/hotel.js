3f4cc7c8aee1a1917dac730e8c08f639e3fc0269
thirthamhanisha <31873069+thirthamhanisha@users.noreply.github.com>
2017-11-16 18:05:36 -0800

    initial changes
    initial changes for sessions

---
+++ kafka-back-end/services/hotel.js
@@ -0,0 +1,52 @@
+var mongo = require("./mongo");
+var mongoURL = "mongodb://localhost:27017/login";
+var bcrypt = require('bcrypt');
+var crypto = require('crypto');
+
+function handle_request(msg, callback){
+
+    //var res = {};
+    console.log("In handle request:" + JSON.stringify(msg));
+    mongo.myconnect(mongoURL, function () {
+        console.log('Connected to mongo at: ' + mongoURL);
+        var coll = mongo.collection('login');
+        key = "273"
+        var hash = crypto.createHmac('sha512', key); //encrytion using SHA512
+        hash.update(msg.password);
+        msg.password = hash.digest('hex');
+        coll.findOne({username: msg.username, password: msg.password}, function (err, user) {
+            if (user) {
+                //  done(null,user/* {username: username, password: password}*/);
+                res.code = "200";
+                res.value = "Success Login";
+                console.log(user);
+                console.log(res.value);
+
+            } else {
+                //    done(null, false);
+                res.code = "401";
+                res.value = "Failed Login";
+                console.log(res.value);
+            }
+            console.log("inside try:" + res);
+            callback(null, res);
+        });
+    })
+    /*   }
+   catch (e){
+       done(e,{});
+   }*/
+    /*if(msg.username == "bhavan@b.com" && msg.password =="a"){
+        res.code = "200";
+        res.value = "Success Login";
+
+    }
+    else{
+        res.code = "401";
+        res.value = "Failed Login";
+    }*/
+    /*console.log("outside try:" + res);
+    callback(null, res);*/
+}
+
+exports.handle_request = handle_request;
