e02afdb1df07b21b5593253c9d277b8afa209e87
Pooja-SD <32438760+Pooja-SD@users.noreply.github.com>
2017-11-23 01:15:47 -0800

    Update server.js

--- kafka-back-end/server.js
+++ kafka-back-end/server.js
@@ -1,7 +1,6 @@
 var connection =  new require('./kafka/Connection');
 
 var car = require('./services/car');
-
 var carDes = require('./services/carDes');
 var bookCar = require('./services/bookCar');
 var payCar = require('./services/payCar');
@@ -15,13 +14,11 @@ var producer = connection.getProducer();
 
 console.log('server is running');
 
-
-
-consumer_list.on('message', function (message) {
-    console.log('message received');
+consumer_car.on('message', function (message) {
+    console.log('message received consumer_car');
     console.log(JSON.stringify(message.value));
     var data = JSON.parse(message.value);
-    list.handle_request(data.data, function(err,res){
+    car.handle_request(data.data, function(err,res){
         console.log('after handle'+res);
         var payloads = [
             { topic: data.replyTo,
@@ -39,74 +36,8 @@ consumer_list.on('message', function (message) {
     });
 });
 
-
-consumer_signup.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    signup.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-
-consumer_hotel.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    hotel.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-consumer_hotelDes.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    hotelDes.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-*/
 consumer_carDes.on('message', function (message) {
-    console.log('message received');
+    console.log('message received consumer_carDesc');
     console.log(JSON.stringify(message.value));
     var data = JSON.parse(message.value);
     carDes.handle_request(data.data, function(err,res){
@@ -128,7 +59,7 @@ consumer_carDes.on('message', function (message) {
 });
 
 consumer_bookCar.on('message', function (message) {
-    console.log('message received');
+    console.log('message received consumer_bookCar');
     console.log(JSON.stringify(message.value));
     var data = JSON.parse(message.value);
     bookCar.handle_request(data.data, function(err,res){
@@ -149,29 +80,9 @@ consumer_bookCar.on('message', function (message) {
     });
 });
 
-consumer_car.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    car.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
+
 consumer_payCar.on('message', function (message) {
-    console.log('message received');
+    console.log('message received consumer_payCar');
     console.log(JSON.stringify(message.value));
     var data = JSON.parse(message.value);
     payCar.handle_request(data.data, function(err,res){
@@ -191,180 +102,7 @@ consumer_payCar.on('message', function (message) {
         return;
     });
 });
-/*consumer3.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    upload.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
 
-consumer4.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    share.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
+       
+            
 
-consumer5.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    star.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-
-consumer6.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    getstar.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-
-consumer7.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    delstar.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-
-consumer8.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    editprofile.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-
-consumer9.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    createfolder.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
-
-consumer10.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    creategroup.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-
-});
-*/
