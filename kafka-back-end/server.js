a653a0a14e17f56f716266f65d68ffdd7a18f546
Pooja-SD <32438760+Pooja-SD@users.noreply.github.com>
2017-11-23 01:10:48 -0800

    Update server.js

--- kafka-back-end/server.js
+++ kafka-back-end/server.js
@@ -1,57 +1,20 @@
 var connection =  new require('./kafka/Connection');
-//var login = require('./services/login');
-//var list = require('./services/listdir');
-//var signup = require('./services/signup');
-//var hotel = require('./services/hotel');
+
 var car = require('./services/car');
-//var hotelDes = require('./services/hotelDes');
+
 var carDes = require('./services/carDes');
 var bookCar = require('./services/bookCar');
 var payCar = require('./services/payCar');
-//var topic_name = 'login_topic';
-//var consumer = connection.getConsumer(topic_name);
-//var consumer_login = connection.getConsumer('login_topic');
-//var consumer_list = connection.getConsumer('list_topic');
-//var consumer_signup = connection.getConsumer('signup_topic');
-//var consumer_hotel = connection.getConsumer('hotel_topic');
-//var consumer_hotelDes = connection.getConsumer('hotelDes_topic');
+
 var consumer_car = connection.getConsumer('car_topic');
 var consumer_carDes = connection.getConsumer('carDes_topic');
 var consumer_bookCar=connection.getConsumer('bookCar_topic');
 var consumer_payCar=connection.getConsumer('payCar_topic');
-/*var consumer3 = connection.getConsumer('upload_topic');
-var consumer4 = connection.getConsumer('share_topic');
-var consumer5 = connection.getConsumer('star_topic');
-var consumer6 = connection.getConsumer('getstar_topic');
-var consumer7 = connection.getConsumer('delstar_topic');
-var consumer8 = connection.getConsumer('profile_topic');
-var consumer9 = connection.getConsumer('folder_topic');
-var consumer10 = connection.getConsumer('group_topic');*/
 
 var producer = connection.getProducer();
 
 console.log('server is running');
-/*consumer_login.on('message', function (message) {
-    console.log('message received');
-    console.log(JSON.stringify(message.value));
-    var data = JSON.parse(message.value);
-    login.handle_request(data.data, function(err,res){
-        console.log('after handle'+res);
-        var payloads = [
-            { topic: data.replyTo,
-                messages:JSON.stringify({
-                    correlationId:data.correlationId,
-                    data : res
-                }),
-                partition : 0
-            }
-        ];
-        producer.send(payloads, function(err, data){
-            console.log(data);
-        });
-        return;
-    });
-});
+
 
 
 consumer_list.on('message', function (message) {
